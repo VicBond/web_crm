aafcaa3107c162b555f1f0857e55aa9d83d9eee9
Victor Bondarenko <nirasimha@gmail.com>
2022-06-20 13:28:20 -0400

    add app get /

--- index.js
+++ index.js
@@ -3,4 +3,10 @@ const express = require('express');
 const app = express();
 const port = 5000;
 
+app.get('/', (req, res) => {
+  res.status(200).json({
+    message: 'Working on port ' + port
+  })
+})
+
 app.listen(port, () => console.log('Server has been started'))
