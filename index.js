92bfc53618f09a801814c611895074fa50a455ac
Victor Bondarenko <nirasimha@gmail.com>
2022-06-21 11:54:56 -0400

    add nodemon

--- index.js
+++ index.js
@@ -8,4 +8,4 @@ const port = process.env.PORT || 5000;
 //   })
 // })
 
-app.listen(port, () => console.log('Server has been started'))
+app.listen(port, () => console.log(`Server has been started on ${port}`))
