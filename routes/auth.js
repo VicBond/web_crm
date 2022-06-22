2e8554afe3d1075c0f51b9af3c2f3c4d2ecfbbe2
Victor Bondarenko <nirasimha@gmail.com>
2022-06-22 08:19:24 -0400

    add register controller in get route

--- routes/auth.js
+++ routes/auth.js
@@ -5,5 +5,8 @@ const router = express.Router();
 // localhost:5000/api/auth/login
 router.get('/login', controller.login);
 
+// localhost:5000/api/auth/register
+router.get('/register', controller.register);
+
 
 module.exports = router
