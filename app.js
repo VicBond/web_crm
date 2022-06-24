2b8cd1be13ca7ee3af4d2c04879b65efdcf627d7
Victor Bondarenko <nirasimha@gmail.com>
2022-06-24 08:32:52 -0400

    add app use morgan & cors

--- app.js
+++ app.js
@@ -10,9 +10,11 @@ const positionRoutes = require('./routes/position');
 const app = express();
 
 
+
+app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
-
+app.use(cors());
 
 app.use('/api/auth', authRoutes);
 app.use('/api/analytics', analyticsRoutes);
