215eec02953f288fee4ff21da69de2fb6ae370ef
Victor Bondarenko <nirasimha@gmail.com>
2022-06-29 12:36:45 -0400

    add User controller

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,6 @@
+const User = require('../models/User');
+
+
 module.exports.login = function(req, res) {
   res.status(200).json({
     login: {
@@ -8,7 +11,12 @@ module.exports.login = function(req, res) {
 }
 
 module.exports.register = function(req, res) {
-  res.status(200).json({
-    register: 'from controller',
+  // res.status(200).json({
+  //   register: 'from controller',
+  // })
+  const user = new User({
+    email: req.body.email,
+    password: req.body.password
   })
+  
 }
