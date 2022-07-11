a948fb0b1437eb3bdca60ff71f4ad0368a63859b
Victor Bondarenko <nirasimha@gmail.com>
2022-07-11 08:45:08 -0400

    add try catch schema to all export modules

--- controllers/position.js
+++ controllers/position.js
@@ -1,15 +1,34 @@
 const position = require('../models/Position');
 const errorHandler = require('../utils/errorHandler');
 
-module.exports.getByCategoryId = function(req, res) {
-  
+module.exports.getByCategoryId = async function(req, res) {
+  try {
+
+  } catch (err) {
+    errorHandler(res, err);
+  }
 }
+
 module.exports.create = function(req, res) {
+  try {
 
+  } catch (err) {
+    errorHandler(res, err);
+  }
 }
+
 module.exports.remove = function(req, res) {
+  try {
 
+  } catch (err) {
+    errorHandler(res, err);
+  }
 }
-module.exports.update = function(req, res) {
 
+module.exports.update = function(req, res) {
+  try {
+
+  } catch (err) {
+    errorHandler(res, err);
+  }
 }
