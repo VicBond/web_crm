2b904daf873ddcd5aef0f7b56d5494518059988c
Victor Bondarenko <nirasimha@gmail.com>
2022-07-12 08:23:01 -0400

    add new position method in create module exports

--- controllers/position.js
+++ controllers/position.js
@@ -13,9 +13,15 @@ module.exports.getByCategoryId = async function(req, res) {
   }
 }
 
-module.exports.create = function(req, res) {
+module.exports.create = async function(req, res) {
   try {
+    const position = await new Position({
+      name: req.body.name,
+      cost: req.body.cost,
+      category: req.body.category,
+      user: req.user.id,
 
+    }).save()
   } catch (err) {
     errorHandler(res, err);
   }
