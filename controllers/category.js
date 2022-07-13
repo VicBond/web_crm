7d2eb33e0fb1a791f2c9bd0f99844ede5a657fe8
Victor Bondarenko <nirasimha@gmail.com>
2022-07-13 08:01:48 -0400

    add trycatch scheme to all cotroller methods

--- controllers/category.js
+++ controllers/category.js
@@ -2,21 +2,41 @@ const Category = require('../models/Category');
 const errorHandler = require('../utils/errorHandler');
 
 module.exports.getAll = function(req, res) {
-  
+  try {
+    
+  } catch (error) {
+    errorHandler(res, error);
+  }
 }
 
 module.exports.getById = function(req, res) {
-
+  try {
+    
+  } catch (error) {
+    errorHandler(res, error);
+  }
 }
 
 module.exports.remove = function(req, res) {
-
+  try {
+    
+  } catch (error) {
+    errorHandler(res, error);
+  }
 }
 
 module.exports.create = function(req, res) {
-
+  try {
+    
+  } catch (error) {
+    errorHandler(res, error);
+  }
 }
 
 module.exports.update = function(req, res) {
-
+  try {
+    
+  } catch (error) {
+    errorHandler(res, error);
+  }
 }
