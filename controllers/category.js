e6aa02fb34e1a66eccce2b049513d8335fbd8937
Victor Bondarenko <nirasimha@gmail.com>
2022-07-13 08:00:20 -0400

    require Category & errorHandler

--- controllers/category.js
+++ controllers/category.js
@@ -1,15 +1,22 @@
+const Category = require('../models/Category');
+const errorHandler = require('../utils/errorHandler');
+
 module.exports.getAll = function(req, res) {
   
 }
+
 module.exports.getById = function(req, res) {
 
 }
+
 module.exports.remove = function(req, res) {
 
 }
+
 module.exports.create = function(req, res) {
 
 }
+
 module.exports.update = function(req, res) {
 
 }
