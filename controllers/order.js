4278a12b9c96720b3710774722e81ec02f3016a4
Victor Bondarenko <nirasimha@gmail.com>
2022-07-19 09:21:34 -0400

    add Order model and errorHandler

--- controllers/order.js
+++ controllers/order.js
@@ -1,6 +1,9 @@
+const Order = require('../models/Order');
+const errorHandler = require('../utils/errorHandler');
+
 module.exports.getAll = function(req, res) {
   
 }
-module.exports.create = function(req, res) {
+module.exports.create = async function(req, res) {
 
 }
