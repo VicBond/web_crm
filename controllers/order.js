059c2111efeee5c349093b655cfeae3f6577e5bf
Victor Bondarenko <nirasimha@gmail.com>
2022-07-19 09:27:18 -0400

    add create new order

--- controllers/order.js
+++ controllers/order.js
@@ -7,6 +7,14 @@ module.exports.getAll = function(req, res) {
 module.exports.create = async function(req, res) {
  try {
    
+   const order = await new Order({
+    list: req.body.list,
+    user: req.user.id,
+   
+
+   }).save();
+
+   res.status(201).json(order);
  } catch (error) {
    errorHandler(res, error)
  }
