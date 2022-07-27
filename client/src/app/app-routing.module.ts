593a66f4ea3bee90a580e335954a9aacbf34f54f
Victor Bondarenko <nirasimha@gmail.com>
2022-07-27 09:16:04 -0400

    add register path

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from "@angular/router";
 import { AuthLayoutComponent } from "./shared/layouts/auth-layout/auth-layout.component";
 import { SiteLayoutComponent } from "./shared/layouts/site-layout/site-layout.component";
 import { LoginPageComponent } from "./login-page/login-page.component";
+import { RegisterPageComponent } from "./register-page/register-page.component";
 
 const routes: Routes = [
   {
     path: '', component: AuthLayoutComponent, children: [
-      {path: 'login', component: LoginPageComponent}
+      {path: 'login', component: LoginPageComponent},
+      {path: 'register', component: RegisterPageComponent}
     ]
   },
   {
