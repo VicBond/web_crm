f7922201a71c54939cee44a1ec55b65d39cf81d6
Victor Bondarenko <nirasimha@gmail.com>
2022-07-27 08:52:01 -0400

    add Auth and Site layouts to routes

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,9 +1,20 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
-import { LoginPageComponent } from "./login-page/login-page.component";
+import { AuthLayoutComponent } from "./shared/layouts/auth-layout/auth-layout.component";
+import { SiteLayoutComponent } from "./shared/layouts/site-layout/site-layout.component";
+// import { LoginPageComponent } from "./login-page/login-page.component";
 
 const routes: Routes = [
-  {path: 'login', component: LoginPageComponent}
+  {
+    path: '', component: AuthLayoutComponent, children: [
+
+    ]
+  },
+  {
+    path: '', component: SiteLayoutComponent, children: [
+
+    ]
+  }
 ];
 
 @NgModule({
