bd8f35205098c6bf496c473854cbb69ad807f182
Victor Bondarenko <nirasimha@gmail.com>
2022-07-27 08:46:56 -0400

    add site-layout comp

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { AuthLayoutComponent } from './shared/layouts/auth-layout/auth-layout.component';
+import { SiteLayoutComponent } from './shared/layouts/site-layout/site-layout.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginPageComponent,
-    AuthLayoutComponent
+    AuthLayoutComponent,
+    SiteLayoutComponent
   ],
   imports: [
     BrowserModule,
