5899559c6e4a11978945e725f35e3f7abb344d1b
Victor Bondarenko <nirasimha@gmail.com>
2022-07-27 08:53:28 -0400

    add login path

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -2,12 +2,12 @@ import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { AuthLayoutComponent } from "./shared/layouts/auth-layout/auth-layout.component";
 import { SiteLayoutComponent } from "./shared/layouts/site-layout/site-layout.component";
-// import { LoginPageComponent } from "./login-page/login-page.component";
+import { LoginPageComponent } from "./login-page/login-page.component";
 
 const routes: Routes = [
   {
     path: '', component: AuthLayoutComponent, children: [
-
+      {path: 'login', component: LoginPageComponent}
     ]
   },
   {
