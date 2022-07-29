4a0515ee5cc1a60df5749b2c6fcdb74df3064f5d
Victor Bondarenko <nirasimha@gmail.com>
2022-07-29 07:50:33 -0400

    add private HttpClient as constructor

--- client/src/app/shared/services/auth.service.ts
+++ client/src/app/shared/services/auth.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from "@angular/core";
+import { User } from "../interfaces";
 
 @Injectable()
 
 export class AuthService {
+
+  constructor(private http: HttpClient) {
+
+  }
+
   register() {}
 
-  login() {}
+  login(user: User) {
+
+  }
 }
