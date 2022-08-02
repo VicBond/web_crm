5d371d20690d27b84d853fb7ea3a3cc878764813
Victor Bondarenko <nirasimha@gmail.com>
2022-08-02 11:23:24 -0400

    add AuthService to constructor

--- client/src/app/login-page/login-page.component.ts
+++ client/src/app/login-page/login-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../shared/services/auth.service';
 
 @Component({
   selector: 'app-login-page',
@@ -10,7 +11,7 @@ export class LoginPageComponent implements OnInit {
 
   form!: FormGroup
 
-  constructor() { }
+  constructor(private auth: AuthService) { }
 
   ngOnInit() {
     this.form = new FormGroup({
@@ -20,6 +21,16 @@ export class LoginPageComponent implements OnInit {
   }
 
   onSubmit() {
-
+    // const user = {
+    //   email: this.form.value.email,
+    //   password: this.form.value.password
+    // }
+    this.auth.login(this.form.value).subscribe(
+      () => console.log('login successful'),
+      error => {
+        console.warn(error)
+      }
+    )
   }
+
 }
