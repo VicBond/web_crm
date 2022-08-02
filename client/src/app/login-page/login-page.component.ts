1c7409a7ec04dbb1f99ffec4a50cb5f2cbb40527
Victor Bondarenko <nirasimha@gmail.com>
2022-08-02 11:37:34 -0400

    add Router/ ActivatedRoute services

--- client/src/app/login-page/login-page.component.ts
+++ client/src/app/login-page/login-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { AuthService } from '../shared/services/auth.service';
 
@@ -13,7 +14,7 @@ export class LoginPageComponent implements OnInit, OnDestroy {
   form!: FormGroup;
   aSub!: Subscription;
 
-  constructor(private auth: AuthService) { }
+  constructor(private auth: AuthService, private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit() {
     this.form = new FormGroup({
