e05e28a8d58a06cef546de7d5d578024c4ea52fc
Victor Bondarenko <nirasimha@gmail.com>
2022-08-04 08:20:36 -0400

    add AuthService constructor

--- client/src/app/shared/classes/auth.guard.ts
+++ client/src/app/shared/classes/auth.guard.ts
@@ -1,11 +1,17 @@
 import { Injectable } from "@angular/core";
 import { ActivatedRouteSnapshot, CanActivate, CanActivateChild, RouterStateSnapshot } from "@angular/router";
 import { Observable } from "rxjs";
+import { AuthService } from "../services/auth.service";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate, CanActivateChild {
+
+  constructor(private auth: AuthService) {
+
+  }
+
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> {
 
   }
