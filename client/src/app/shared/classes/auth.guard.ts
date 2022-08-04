af160803851ac4b0270a3298f182999179ca7047
Victor Bondarenko <nirasimha@gmail.com>
2022-08-04 08:25:01 -0400

    add check auth by isAuthenticated/ add redirect to register

--- client/src/app/shared/classes/auth.guard.ts
+++ client/src/app/shared/classes/auth.guard.ts
@@ -1,6 +1,6 @@
 import { Injectable } from "@angular/core";
-import { ActivatedRouteSnapshot, CanActivate, CanActivateChild, RouterStateSnapshot } from "@angular/router";
-import { Observable } from "rxjs";
+import { ActivatedRouteSnapshot, CanActivate, CanActivateChild, Router, RouterStateSnapshot } from "@angular/router";
+import { Observable, of } from "rxjs";
 import { AuthService } from "../services/auth.service";
 
 @Injectable({
@@ -8,12 +8,21 @@ import { AuthService } from "../services/auth.service";
 })
 export class AuthGuard implements CanActivate, CanActivateChild {
 
-  constructor(private auth: AuthService) {
+  constructor(private auth: AuthService,
+    private router: Router) {
 
   }
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> {
-
+    if (this.auth.isAuthenticated()) {
+      return of(true);
+    } else {
+      this.router.navigate(['/login'], {
+        queryParams: {
+          accessDenied: true
+        }
+      })
+    }
   }
 
   canActivateChild(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> {
