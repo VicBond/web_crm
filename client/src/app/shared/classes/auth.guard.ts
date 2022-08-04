746cf70a9d9d9734e477ccea167a9b359c886bde
Victor Bondarenko <nirasimha@gmail.com>
2022-08-04 08:19:04 -0400

    add @Injectable

--- client/src/app/shared/classes/auth.guard.ts
+++ client/src/app/shared/classes/auth.guard.ts
@@ -1,6 +1,10 @@
+import { Injectable } from "@angular/core";
 import { ActivatedRouteSnapshot, CanActivate, CanActivateChild, RouterStateSnapshot } from "@angular/router";
 import { Observable } from "rxjs";
 
+@Injectable({
+  providedIn: 'root'
+})
 export class AuthGuard implements CanActivate, CanActivateChild {
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> {
 
