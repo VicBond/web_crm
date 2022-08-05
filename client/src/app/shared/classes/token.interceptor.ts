7ea53e2906d9a98a537dd804536226f419aa6c92
Victor Bondarenko <nirasimha@gmail.com>
2022-08-05 08:14:01 -0400

    add constructor method to export class tokeninterceptor

--- client/src/app/shared/classes/token.interceptor.ts
+++ client/src/app/shared/classes/token.interceptor.ts
@@ -1,6 +1,9 @@
 import { Injectable } from "@angular/core";
+import { AuthService } from "../services/auth.service";
 
 @Injectable()
 export class TokenInterceptor {
+constructor(private auth: AuthService) {
 
+}
 }
