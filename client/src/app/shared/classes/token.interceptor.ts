177c5699ac91e66c04e4c32db02ab34608ca53de
Victor Bondarenko <nirasimha@gmail.com>
2022-08-05 08:17:50 -0400

    add intercept method with httprequest

--- client/src/app/shared/classes/token.interceptor.ts
+++ client/src/app/shared/classes/token.interceptor.ts
@@ -1,9 +1,15 @@
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
 import { AuthService } from "../services/auth.service";
 
 @Injectable()
-export class TokenInterceptor {
+export class TokenInterceptor implements HttpInterceptor {
 constructor(private auth: AuthService) {
 
 }
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+
+    return next.handle(req)
+  }
 }
